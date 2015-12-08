99b925b1b74a9829dcc18772a00be628db693c00
Zibi Braniecki <gandalf@mozilla.com>
2015-12-08 13:59:11 -0500

    add mozIntl objects

--- src/lib/objects.js
+++ src/lib/objects.js
@@ -9,14 +9,12 @@ export const knownObjects = {
 
       return Intl.DateTimeFormat(navigator.languages, resolvedOptions);
     },
-
     isTypeAffected: function (reason) {
       return [
         'timeformatchange',
         'languagechange',
         'moztimechange'].includes(reason);
     },
-
     isAffected: function (reason, options) {
       if (reason === 'languagechange' || reason === 'moztimechange') {
         return true;
@@ -35,11 +33,9 @@ export const knownObjects = {
 
       return Intl.NumberFormat(navigator.languages, resolvedOptions);
     },
-
     isTypeAffected: function (reason) {
       return ['languagechange'].includes(reason);
     },
-
     isAffected: function (reason, options) {
       return reason === 'languagechange';
     }
@@ -48,11 +44,45 @@ export const knownObjects = {
     create: function(options) {
       return Intl.Collator(navigator.languages, options);
     },
-
     isTypeAffected: function (reason) {
       return ['languagechange'].includes(reason);
     },
+    isAffected: function(obj, reason) {
+      return reason === 'languagechange';
+    }
+  },
+  mozdatetime: {
+    create: function(options) {
+      const resolvedOptions = Object.assign({}, options);
 
+      if (resolvedOptions.hour) {
+        resolvedOptions.hour12 = navigator.mozHour12;
+      }
+
+      return mozIntl.DateTimeFormat(navigator.languages, resolvedOptions);
+    },
+    isTypeAffected: function (reason) {
+      return [
+        'timeformatchange',
+        'languagechange',
+        'moztimechange'].includes(reason);
+    },
+    isAffected: function(obj, reason) {
+      if (reason === 'languagechange' || reason === 'moztimechange') {
+        return true;
+      }
+
+      if (reason === 'timeformatchange') {
+        return 'hour' in options;
+      }
+
+      return false;
+    }
+  },
+  mozduration: {
+    create: function(options) {
+      return mozIntl.DurationFormat(navigator.languages, options);
+    },
     isAffected: function(obj, reason) {
       return reason === 'languagechange';
     }
